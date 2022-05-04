cd736bca539312dffde99538ca4a979e216849fd
Dominik Robert <dominikrobert@gmx.de>
2022-05-04 21:04:53 +0200

    build: Prepare the pipeline and try to run for the first time

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ package main
 
 import "github.com/dominik-robert/kubectl-switch/cmd"
 
+var (
+	Version   = "1.0.0"
+	BuildTime = "2015-08-01 UTC"
+	GitHash   = ""
+)
+
 func main() {
 	cmd.Execute()
 }
